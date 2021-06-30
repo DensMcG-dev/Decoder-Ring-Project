2ea7eee2b76dbb9ec000041d12c5e8dbbbf1d43b
DensMcG-dev <84361651+DensMcG-dev@users.noreply.github.com>
2021-06-30 12:30:39 -0600

    Update caesar.js

--- Project_Decoder_Ring_1/Project_Decoder_Ring_1/src/caesar.js
+++ Project_Decoder_Ring_1/Project_Decoder_Ring_1/src/caesar.js
@@ -6,13 +6,14 @@
 const caesarModule = (function () {
 
   function caesar(input, shift, encode = true) {
-    let lowerCase = input.toLowerCase();
+    let lowerCase = input.toLowerCase(); //changes all letters to lowercase, per directions to ignore capital letters
     const letterArray = Array.from(lowerCase);
     const asciiValueArray =[];
     const finalArray = [];
     if(!shift || shift === 0 || shift < -25 || shift > 25) {
       return false;
     }
+    //checks if shift input is within correct parameters
     
     for(let i = 0; i < letterArray.length; i++) {
       if(letterArray[i] === " ") {
@@ -23,24 +24,25 @@ const caesarModule = (function () {
         asciiValueArray.push(ascii);
       }
   }
-    
+    //constructs array of ASCII value codes for each characters
 
     if(!encode) {shift = -shift};
+    //checks if decoding, and if so changes shift value
 
      for(let j = 0; j < asciiValueArray.length; j++) {
       if(asciiValueArray[j] !== 32 && asciiValueArray[j] > 96 && asciiValueArray[j] < 123) {
         if(asciiValueArray[j] + shift < 123 && asciiValueArray[j] + shift > 96) {
           const fromCharCode = String.fromCharCode(asciiValueArray[j] + shift);
           finalArray.push(fromCharCode);
-        }
+        }// checks if a shift will remain within the alphabet properly, and if so shifts
         else if(asciiValueArray[j] + shift > 122) {
           const fromCharCode = String.fromCharCode(asciiValueArray[j] - 26 + shift);
           finalArray.push(fromCharCode);
-        }
+        }//checks if a shift will move above 'z', and if so wraps around to 'a'
         else if(asciiValueArray[j] + shift < 97) {
           const fromCharCode = String.fromCharCode(asciiValueArray[j] + 26 + shift);
           finalArray.push(fromCharCode);
-        }
+        }//checks if a shift will move below 'a', and if so wraps around to 'z'
       }
       else if(asciiValueArray[j] === 32) {finalArray.push(" ")}
       else if(asciiValueArray[j] < 97) {
@@ -48,7 +50,7 @@ const caesarModule = (function () {
        }
       else if(asciiValueArray[j] > 122) {
         finalArray.push(String.fromCharCode(asciiValueArray[j]))
-       }
+       }//checks if a given character to shift is not a standard alphabet letter, and if so simply returns the character
     }
 
 return finalArray.join('');
